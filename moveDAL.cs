1ca1827c2c7a708a1389d33777e52084480ce7df
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-19 16:02:16 +0300

    Add files via upload

---
+++ moveDAL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace YilanOyunu
+{
+    internal class moveDAL
+    {
+        private bool move;
+        private int mouseX;
+        private int mouseY;
+        private Form targetForm;
+
+        public moveDAL(Form form)
+        {
+            targetForm = form;
+        }
+
+        public void MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        public void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                targetForm.SetDesktopLocation(Control.MousePosition.X - mouseX, Control.MousePosition.Y - mouseY);
+            }
+        }
+
+        public void MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+         public void EnableControlDrag(Control control)
+        {
+            control.MouseDown += MouseDown;
+            control.MouseMove += MouseMove;
+            control.MouseUp += MouseUp;
+        }
+    }
+}
+    
+
